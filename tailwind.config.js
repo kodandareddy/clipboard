bbb5a442d9b0ac3ef4cff9b3d4eb679c774ed096
kodanda reddy <kodandareddy21@gmail.com>
2022-06-25 12:09:36 +0530

    clipboard project completed

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Bai Jamjuree', 'sans-serif'],
+    },
+    extend: {
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+    },
+  },
+  plugins: [],
+}
